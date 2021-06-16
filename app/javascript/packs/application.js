6b0cca5ec0c5c23fb215d757dcb8e3650a260697
Jonathas Andrade <Jonathas.andrade5@gmail.com>
2021-06-16 15:11:34 -0300

    fix: remove tailwind conflicts with sass

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,7 @@ import Turbolinks from "turbolinks";
 import * as ActiveStorage from "@rails/activestorage";
 import "channels";
 
-import "stylesheets/application.scss";
+import "stylesheets/application.css";
 import "./main";
 import "./coins";
 import "./navbar";
